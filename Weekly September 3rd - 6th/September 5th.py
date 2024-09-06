51a39e7473fba180645536b4e3a9f272f513af44
KTvyen <katenguyen08.az@icloud.com>
2024-09-06 08:15:51 -0700

    exported from Skill Struck

---
+++ Weekly September 3rd - 6th/September 5th.py
@@ -0,0 +1,51 @@
+#number = input("enter a full number") 
+#len_number = len(number)
+#for x in number:
+	#print(number[len_number -1])
+
+
+#number = input("enter a full number")
+#len_number = len(number)
+#sub_number = 0
+#While x != number:
+#for x in number:
+	#print(number[len_number - sub_number])
+	#sub_number += 1
+
+
+#num = int(input("Enter in a full number. No decimals ")) 
+#ans = 0 
+#def count_bits(n):
+ 	#ans = 0 
+#for x in n:
+    #if x % 2 == 0 and x != 0: 
+        #ans += 1 
+    #elif x % 2 == 1 and x != 1: 
+        #ans += 0
+    #elif x == 1: 
+        #ans += 1 
+#count_bits(num) 
+#print(ans)
+
+#Number = int(input("number "))
+#Binary_number = number.0b
+#nBinary_number = str(Binary_number)
+
+
+#Number = int(input("number "))
+#loop = 0
+#ans = ""
+#ans = number % 2
+#while loop < 11:
+	#ans += number % 2 
+	#loop =+ 1
+
+#Number = int(input("number "))
+#loop = 0
+#ans = number % 2
+#while loop < 11:
+	#for x in number:
+	#ans += n % 2 
+	#loop =+ 1
+#print(ans)
+
