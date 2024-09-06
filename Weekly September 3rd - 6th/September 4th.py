95f6043fb8455b48705d090a3ebc860782517954
KTvyen <katenguyen08.az@icloud.com>
2024-09-06 08:15:50 -0700

    exported from Skill Struck

---
+++ Weekly September 3rd - 6th/September 4th.py
@@ -0,0 +1,38 @@
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#............................Kata................................#
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+
+#***********************************#
+#           Projects             #
+#***********************************#
+
+
+#8 kyu
+#Multiply
+#Python:
+#def multiply(a, b):
+    #return a * b
+
+#8 kyu
+#Even or Odd
+#Python:
+#def even_or_odd(number):
+    #if number % 2 == 0: 
+        #return("Even")
+    #else:
+        #return("Odd")
+    
+
+#even_or_odd(-2)
+
+#num = int(input(""))
+#answer = 1
+#def make_negative( number ):
+    #if number / -1 > 0:
+        #asnwer = number
+        #print(int(answer))
+    #else:
+        #answer = number / -1
+        #print(int(answer))
+    
+#make_negative(num)
