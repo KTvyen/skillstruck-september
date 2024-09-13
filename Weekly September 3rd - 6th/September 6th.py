0d8532196bc5848c46207d240ca8d03228589412
KTvyen <katenguyen08.az@icloud.com>
2024-09-13 10:03:11 -0700

    exported from Skill Struck

--- Weekly September 3rd - 6th/September 6th.py
+++ Weekly September 3rd - 6th/September 6th.py
@@ -1,1 +1,24 @@
-Stuff
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#.............Looping Through A Dictionary Continued............#
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+
+#***********************************#
+#           Projects             #
+#***********************************#
+
+#looping through a dictionary is already super cool but when combined with other code like and if statement it becomes even more useful !
+
+#for example 
+classmates = {
+    "Billy" : 8,
+    "Vance" : 12,
+    "Alice" : 10,
+    "Eliza" : 15,
+    "Xaxier" : 6,
+}
+
+for x in classmates:
+    if x == "Eliza":
+        print("this person wiches to be annoymous")
+    else:
+        print(x)
