a95c58d20a761d69faf78c5093958be8a9035275
KTvyen <katenguyen08.az@icloud.com>
2024-09-13 10:03:15 -0700

    exported from Skill Struck

---
+++ Weekly September 9th - 13th/September 11th.py
@@ -0,0 +1,36 @@
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#............Looping Through A Dictionary Continued............#
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+
+#periods = int(input("How many class periods do you hae?"))
+#schedule = {}
+#for i in range(periods):
+    #subject = input("What subject?")
+    #num_people = input("How many people are in your " + subject + " class?")
+    #if subject not in schedule:
+        #schedule[subject] = num_people
+#print(schedule)
+
+
+
+#ride = { "jess" : 165, "tim" : 145, "benson" : 120, "jayla" : 100, "sherb" : 98}
+#for x in ride.values():
+    #if x >= 100:
+        #print("This person is tall enough to ride.")
+    #else:
+        #print("This person is too short to ride.")
+
+
+#***********************************#
+#           Projects             #
+#***********************************#
+
+#dictionary = { 7: "first", 3: "second", 4: "third", 8: "fourth", 9: "fifth" }
+
+#my_list = [int(n) for n in input().split()]
+
+#for x in my_list:
+    #if x in dictionary:
+        #print("Yes")
+    #else:
+        #print("No")
